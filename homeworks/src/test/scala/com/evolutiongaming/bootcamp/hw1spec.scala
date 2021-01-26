13be0c2b1984c6a564c54ca3a670930c71e946f8
Vladislavs Burakovs <vladislavs.burakovs@gmail.com>
2021-01-26 20:52:15 +0200

    Reverted rename Updated build.sbt

---
+++ homeworks/src/test/scala/com/evolutiongaming/bootcamp/hw1spec.scala
@@ -0,0 +1,18 @@
+
+package com.evolutiongaming.bootcamp.basics
+
+import Basics._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers._
+import org.scalacheck.Arbitrary._
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+
+class BasicsSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks {
+  "lcm" should "derive least common multiple of 213 and 109" in {
+    lcm(213, 109) shouldEqual 23217
+  }
+
+  "gcd" should "derive greatest common divisor of 24 and 12" in {
+    gcd(24, 18) shouldEqual 6
+  }
+}
