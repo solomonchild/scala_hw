41a6005f158ef32809ffd46cff86adf97523dbca
Vladislavs Burakovs <vladislavs.burakovs@gmail.com>
2021-02-03 07:41:56 +0200

    match -> map

--- src/main/scala/com/evolutiongaming/bootcamp/basics/hw1.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/basics/hw1.scala
@@ -1,7 +1,5 @@
 package com.evolutiongaming.bootcamp.basics
 
-import java.security.InvalidParameterException
-
 object Basics {
   def gcd(a: Int, b: Int): Option[Int] = (a, b) match {
     case (0, 0) => None
@@ -11,12 +9,6 @@ object Basics {
 
   def lcm(a: Int, b: Int): Option[Int] = {
     if (List(a, b).exists(_ == 0)) None
-    else {
-      val g = gcd(a, b)
-      g match {
-        case None    => None
-        case Some(x) => Some(Math.abs(a * b) / x)
-      }
-    }
+    else gcd(a, b).map(x => Math.abs(a * b) / x)
   }
 }
