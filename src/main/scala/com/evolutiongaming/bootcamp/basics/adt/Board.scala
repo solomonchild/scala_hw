84916e9dc8b894077c7f010d951a2f41f2d0c1e3
Vladislavs Burakovs <vladislavs.burakovs@gmail.com>
2021-02-13 06:46:53 +0200

    Made Board private to allow for validation

--- src/main/scala/com/evolutiongaming/bootcamp/basics/adt/Board.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/basics/adt/Board.scala
@@ -1,3 +1,6 @@
 package com.evolutiongaming.bootcamp.basics.adt
 
-final case class Board(hands: List[Hand])
+final case class Board private (game: Game, hands: List[Hand])
+object Board {
+  def create(game: Game, hands: List[Hand]): Option[Board] = ???
+}
